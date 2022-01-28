f6a327e3831c519f195868c8ae8cb6721e34dfad
Andrew Trice <amtrice@us.ibm.com>
2022-01-28 12:45:34 -0500

    fixed typo

--- src/main.ts
+++ src/main.ts
@@ -42,7 +42,7 @@ async function run(): Promise<void> {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const key: keyof IssueInfo = value as any
 
-      if (key != "displayName" || (result[key] === defined || result[key] === "")) {
+      if (key != "displayName" || (result[key] === undefined || result[key] === "")) {
         core.setOutput(key, result[key] || '')
       }
     })
